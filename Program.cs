f737192baa3b31432e9e6b6224db72aa1668603e
ashraf-mortazavi <ashrafmortazavi18@gmail.com>
2022-09-12 16:27:03 +0430

    AddMiddelware to project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using static PhoneBook.Models.PhoneBooks;
+using PhonBook.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,8 +25,13 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+    
+
 app.Run();
+
+app.UseMiddleware<Mymiddelware>();
